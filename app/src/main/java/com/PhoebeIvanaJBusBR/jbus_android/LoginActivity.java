f162ddd580823997222af37be48b839d11b17388
unknown <phoebe.i.solaiman@gmail.com>
2023-11-17 17:51:05 +0700

    CS08 - Pengecekan Bagian Action Bar

--- app/src/main/java/com/PhoebeIvanaJBusBR/jbus_android/LoginActivity.java
+++ app/src/main/java/com/PhoebeIvanaJBusBR/jbus_android/LoginActivity.java
@@ -2,13 +2,33 @@ package com.PhoebeIvanaJBusBR.jbus_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private TextView registerNow = null;
+    private Button loginButton = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerNow = findViewById(R.id.register_now);
+        loginButton = findViewById(R.id.button);
+        registerNow.setOnClickListener(e->{moveActivity(this, RegisterActivity.class);});
+        loginButton.setOnClickListener(e->{moveActivity(this, MainActivity.class);});
+        getSupportActionBar().hide();
+    }
+    // method untuk melakukan perpindahan activity
+    private void moveActivity(Context ctx, Class<?> cls){
+        Intent intent = new Intent(ctx,cls);
+        startActivity(intent);
+    }
+    private void viewToast(Context ctx, String message){
+        Toast.makeText(ctx,message,Toast.LENGTH_SHORT).show();
     }
 }
