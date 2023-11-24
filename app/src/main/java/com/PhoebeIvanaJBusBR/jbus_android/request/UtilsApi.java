2b8f7778f21e3c32e62ca6745a40790b00af9e63
unknown <phoebe.i.solaiman@gmail.com>
2023-11-24 23:05:18 +0700

    CS 09 - Berhasil Menyelesaikan CS 9

--- app/src/main/java/com/PhoebeIvanaJBusBR/jbus_android/request/UtilsApi.java
+++ app/src/main/java/com/PhoebeIvanaJBusBR/jbus_android/request/UtilsApi.java
@@ -1,6 +1,6 @@
 package com.PhoebeIvanaJBusBR.jbus_android.request;
 public class UtilsApi {
-    public static final String BASE_URL_API = "http://192.168.137.1:5000/";
+    public static final String BASE_URL_API = "http://10.0.2.2:5000/";
     public static BaseApiService getApiService() {
         return
                 RetrofitClient.getClient(BASE_URL_API).create(BaseApiService.class);
